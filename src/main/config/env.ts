0f00a44fd026be02b1c7dab26d207743d337ed4c
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-05-10 03:44:44 -0300

    chore: fix envirionment variables config file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   MONGO_URL: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
   PORT: process.env.NODE_PORT ?? 3000,
-  SALT: process.env.CRYPTO_SALT ?? 12
+  SALT: parseInt(process.env.CRYPTO_SALT ?? '12')
 }
