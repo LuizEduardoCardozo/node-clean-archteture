359d3330b7874e21252bf6caba863b58ca1a44be
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-05-10 03:43:00 -0300

    chore: adds salt variable to environment config file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   MONGO_URL: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
-  PORT: process.env.NODE_PORT ?? 3000
+  PORT: process.env.NODE_PORT ?? 3000,
+  SALT: process.env.CRYPTO_SALT ?? 12
 }
