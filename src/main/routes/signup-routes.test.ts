38b5050bad3cc4b7c7fc3c11b39be6798824ecfd
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-05-10 03:34:25 -0300

    feat: add mongodb connection to signup routes test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,8 +1,21 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 import app from '../config/app'
 
 describe('signup route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  beforeEach(async () => {
+    await MongoHelper.clean()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('should request to singup route', async () => {
     await request(app)
       .post('/api/signup')
